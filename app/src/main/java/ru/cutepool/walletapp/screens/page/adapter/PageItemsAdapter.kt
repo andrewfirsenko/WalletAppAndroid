40bbc14eccbae32ab394673a2686233c9f272fe5
Ilya Gavrilov <llxodz@gmail.com>
2021-03-08 12:43:02 +0300

    Fix

--- app/src/main/java/ru/cutepool/walletapp/screens/page/adapter/PageItemsAdapter.kt
+++ app/src/main/java/ru/cutepool/walletapp/screens/page/adapter/PageItemsAdapter.kt
@@ -13,7 +13,7 @@ import java.math.BigDecimal
 
 class PageItemsAdapter : RecyclerView.Adapter<PageItemsAdapter.ItemViewHolder>() {
 
-    var cards: MutableList<Card> = mutableListOf()
+    private var cards: MutableList<Card> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val itemView =
